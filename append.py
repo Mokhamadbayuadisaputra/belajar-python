be3769463e7c8e83cffcba6260c7787d62fbe28e
MokhamadBayuAdi <mokhamadbayuadisaputra@gmail.com>
2025-06-18 08:56:04 +0700

    ini Append di python

---
+++ append.py
@@ -0,0 +1,6 @@
+# menambah list
+# ada list kosong yang bernama buah,  lalu tambahkan buah yang di beli di pasar : semangka, pisang, apel, durian
+# tambahkan nanas yang baru di beli ke keranjang
+buah = ["Semangka", "Pisang", "Apel", "Durian"]
+buah.append("Nanas")
+print(buah)
