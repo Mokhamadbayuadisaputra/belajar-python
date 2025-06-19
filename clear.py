00b2ef4efc6855fc5e432a5d4669efc445d2f965
MokhamadBayuAdi <mokhamadbayuadisaputra@gmail.com>
2025-06-19 10:24:44 +0700

    ini adalah method clear dalam list di python

---
+++ clear.py
@@ -0,0 +1,7 @@
+# ini adalah method clear dalam python
+# di dalam keranjang terdapat buah lalu makan buah tersebut
+buah = ["Semangka", "Pisang", "Apel", "Durian"]
+buah.clear()
+print(buah)
+
+# output listnya akan kosong karna buah di clear
