c73be3e84650e4fcbdcc31849865760cd74459d7
MokhamadBayuAdi <mokhamadbayuadisaputra@gmail.com>
2025-06-22 10:52:49 +0700

    ini adalah count yang gunanya menghitung jumlah value di dalam list

---
+++ count.py
@@ -0,0 +1,8 @@
+# count di dalam py akan di gunakan untuk menghitung jumlah value yang ada di dalam list
+# contoh 
+
+buah = ["Semangka", "Apel" , "Rambutan", "Pier" , "Nangka", "Jeruk", "Pepaya" , "Pisang" , "Nangka"]
+hitung = buah.count("Nangka")
+print(hitung)
+
+# oytpunya 2 karna yang saya cari (nangka) ada dua
