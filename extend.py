87f461d5438ff3a41891665d1a31645a6041b161
MokhamadBayuAdi <mokhamadbayuadisaputra@gmail.com>
2025-06-23 19:06:59 +0700

    ini adalah extend untuk menggabungkan list

---
+++ extend.py
@@ -0,0 +1,6 @@
+# extend ini adalah penggabungan list satu dengan list lainnya
+# contoh
+keranjang1 = ["Semangka", "Nanas", "Rambutan","Alpukat"]
+keranjang2 = ["Apel", "Pisang", "Durian", "Pepaya"]
+keranjang1.extend(keranjang2)
+print(keranjang1)
--- lopping_for.py
+++ lopping_for.py
@@ -1,3 +1,4 @@
 # ini adalah looping menggunakan for di python
 for i in range(5):
     print(f"Hallo ke- {i}")
+
