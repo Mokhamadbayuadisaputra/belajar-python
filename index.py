cf761891b5f619d692375c3387f2ed4fc2f1df77
MokhamadBayuAdi <mokhamadbayuadisaputra@gmail.com>
2025-06-24 12:00:47 +0700

    mengecek index di dalam list python

---
+++ index.py
@@ -0,0 +1,8 @@
+# ini adalah index yang fungsinya untuk mengetahui index di dalam list
+# contoh
+# di dalam sekolah terdapat ada beberapa ruang yang pertama adalah lab komputer kedua perpustakaan dan ketiga kantin
+# carilah lam komputer ada di index berapa?
+sekolah = ["lab komputer", "Perpustakaan", "Kantin"]
+x = sekolah.index("lab komputer")
+print(x)
+
